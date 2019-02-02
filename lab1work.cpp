28d608a21a531f0c47750c1d6451d04d8c8bd850
GoldLucasino0 <36017448+GoldLucasino0@users.noreply.github.com>
2019-02-02 02:40:16 -0500

    Update lab1work.cpp

--- lab1work.cpp
+++ lab1work.cpp
@@ -1,166 +1,154 @@
 #include <iostream>
-#include <sys/time.h>
-#include <algorithm>
-#include <random>
+#include <vector>
 using namespace std;
 
-int * GenerateRandomArray(int size)
+int * GenerateRandomArray(int len)
 {
-    int * random = new int[size];
-    for(int i=0;i<size;i++)
+    int * random = new int[len];
+    for(int i=0;i<len;i++)
         random[i] = (rand() % 100000);
     return random;
 };
 
-void PrintArray (int * array, int size)
+void PrintArray (int* array, int len)
 {
-    for (int i=0; i<size; i++) 
-        cout<< array[i] << endl;
+        for (int i=0; i<len; i++)
+                cout<< array[i] << endl;
 };
 
-int * DuplicateArray (int* orig, int size)
+
+int * DuplicateArray (int* orig, int len)
 {
-    int * temp = new int[size];
-    for(int i=0;i<size;i++)
+    int * temp = new int[len];
+    for(int i=0;i<len;i++)
         temp[i] = orig[i];
       return temp;
 };
 
-bool IsSorted(int* array, int size)
+
+template <class T>
+void bubblesort (T a[], int len)
 {
-    if (size == 1 || size == 0)
-        return 1;
 
-    if(array[size-1] < array[size-2])
-    return 0;
-};
+     for (int end_index = len-1; end_index>0; end_index--)
+     {
+                for (int i=0; i<=end_index-1;i++)
+                        if (a[i] > a[i+1])
+                                swap(a[i], a[i+1]);
+     }
 
-void BubbleSort(int* array, int size)
-{
-    int i, j;
-    for (i = 0; i < size-1; i++)
-        //  Last i elements are already in place
-        for (j = 0; j < size-i-1; j++)
-            if (array[j] > array[j+1])
-                swap(array[j], array[j+1]);
-};
+}
 
-void SelectionSort(int* array, int size)
+
+template <class T>
+void selectionsort (T a[], int len)
 {
     int i, j, min_idx;
     //  One by one move boundary of unsorted subarray
-    for (i =0; i< size-1; i++)
-        {   
+    for (i =0; i< len-1; i++)
+        {
         //  Find the minimum element in unosrted array
         min_idx = i;
-        for (j = i+1; j <size; j++)
-            if (array[j] < array[min_idx])
+        for (j = i+1; j <len; j++)
+            if (a[j] < a[min_idx])
             min_idx = j;
 
         //  Swap the found minimum element with the first element
-        swap(array[min_idx], array[i]);
+        swap(a[min_idx], a[i]);
         }
 };
 
-void InsertionSort(int* array , int size)
+template <class T>
+void insertionsort (T a[], int len)
 {
     //  Function to sort an array using insertion sort
     int i, key, j;
-    for (i = 1; i < size; i++)
+    for (i = 1; i < len; i++)
     {
-        key = array[i];
+        key = a[i];
         j = i-1;
 
         //  Move elements of array[0.. i-1], that are greater than key, to one position ahead of their current position
-        while (j >= 0 && array[j] > key)
+        while (j >= 0 && a[j] > key)
         {
-            array[j+1] = array[j];
+            a[j+1] = a[j];
             j = j-1;
         }
-        array[j+1] = key;
+        a[j+1] = key;
     }
 };
 
-void MeasuringSortingFunction(int input_size, int run_number)
+template <class T>
+bool IsSorted(T a[], int len)
 {
-struct timeval begin, end;
-double elapsed;
-for (int i=run_number;i<=10;i++)
-{
+        if (len == 1 || len == 0)
+                return true;
 
-    gettimeofday(&begin,NULL);
-//    BubbleSort(array,10);
-    gettimeofday(&end,NULL);
+        for (int i = 1; i < len; i++)
+                if (a[i-1] > a[i])
+                        return false;
 
-elapsed = (end.tv_sec-begin.tv_sec)+((end.tv_usec-begin.tv_usec)/1000000.0);
-
-
-}
-
+        if (a[len-1] < a[len-2])
+                return 0;
 };
+
+
 
 int main()
 {
-int * array = GenerateRandomArray(10);
-int * orig = array;
-int * duplicate = DuplicateArray(orig, 10);
-int * duplicate_2 = DuplicateArray(orig, 10);
+        int * array = GenerateRandomArray(10);
+        int * duplicate = DuplicateArray(array, 10);
+        int * duplicate_2 = DuplicateArray(array, 10);
+        int len;
 
-int size;
+        cout<< "Testing bubble sort" << endl;
+        PrintArray(array,10);
+        bubblesort(array,10);
+        cout<< endl;
+        cout<< "After: " << endl;
+        PrintArray(array,10);
+        if(IsSorted(array,10))
+        {
+                cout<< "BubbleSort passed testing";
+                cout<< endl;
+        }
+        else
+        {
+                cout<< "BubbleSort does not work!" << endl;
+        }
+        cout<< endl;
 
-cout<< "1. Testing the three algorithms" << endl;
-cout<< "Testing bubble sort with an array of size 10:" << endl;
-cout<< "Bubble Sort array:" << endl;
-PrintArray(array,10);
-BubbleSort(array,10);
-cout << endl;
-cout << "After:" << endl;
-PrintArray(array,10);
-if (IsSorted (array,10))
-{
-    cout<< "BubbleSort passed testing!" << endl;
-    cout<< endl;
-}
-else
-{
-    cout<< "BubbleSort does not work!" << endl;
-    cout<< endl;
-}
+        cout<< "Testing selection sort" << endl;
+        PrintArray(duplicate,10);
+        selectionsort(duplicate,10);
+        cout<< endl;
+        cout<< "After: " << endl;
+        PrintArray(duplicate,10);
+        if(IsSorted(array,10))
+        {
+                cout<< "Selection sort passed testing";
+               cout<< endl;
+        }
+        else
+        {
+                cout<< "Selection sort does not work!" << endl;
+        }
+        cout<< endl;
 
-cout<< "Testing selection sort with an array of size 10:" << endl;
-cout<< "Selection Sort array:" << endl;
-PrintArray(duplicate,10);
-SelectionSort(duplicate,10);
-cout << endl;
-cout<< "After:" << endl;
-PrintArray(duplicate,10);
-if (IsSorted (duplicate,10))
-{
-    cout<< "SelectionSort passed testing!" << endl;
-    cout<< endl;
-}
-else
-{
-    cout<< "SelectionSort does not work!" << endl;
-}
-
-cout<< "Testing selection sort with an array of size 10:" << endl;
-cout<< "Insertion Sort array:" << endl;
-PrintArray(duplicate_2,10);
-InsertionSort(duplicate_2,10);
-cout << endl;
-cout << "After:" << endl;
-PrintArray(duplicate_2,10);
-if (IsSorted (duplicate_2,10))
-    cout<< "InsertionSort passed testing!" << endl;
-    else
-    cout << "SelectionSort does not work!" << endl;
-
-
-delete [] array;
-
-//MeasuringSortingFunction(input_size, run_number);
-
-
-return 0;
-}
+        cout<< "Testing insertion sort" << endl;
+        PrintArray(duplicate_2,10);
+        insertionsort(duplicate_2,10);
+        if(IsSorted(array,10))
+        {
+                cout<< "Insertion sort passed testing";
+                cout<< endl;
+        }
+        else
+        {
+                cout<< "Insertion sort does not work!" << endl;
+        }
+        cout<< endl;
+        cout<< "After: " << endl;
+        PrintArray(duplicate_2,10);
+        cout<< endl;
+        return 0;
